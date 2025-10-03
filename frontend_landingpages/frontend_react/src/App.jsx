4b14326469578c88df899f934ac6979e26ecf5b6
Jing Rou <leongjingrou@gmail.com>
2025-10-03 23:04:56 +0800

    fix loginpage route

--- frontend_landingpages/frontend_react/src/App.jsx
+++ frontend_landingpages/frontend_react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import LoginForm from './pages/loginpage';
+import {LoginForm} from './pages/loginpage';
 
 function App() {
   const [count, setCount] = useState(0)
